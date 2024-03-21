2c910524a700b94e6ba98ade3b4a40a23609725a
ashiheimaruyama <45790055+ashiheimaruyama@users.noreply.github.com>
2024-03-21 23:32:58 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-4e205193-e802-4c1e-a272-a46c1b2c2dd2/)
 
 
 echo "$curl_output" >> Update.tsx
